254d2fc75b17b6eeafb47a73a229a5a718fa9472
DHINAKARAN K <kcsdhina2580@gmail.com>
2025-09-02 22:40:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Product from "./components/Product";
+import Search from "./components/Search";
+
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
